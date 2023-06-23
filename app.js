bf194cc1cd6776f4340bfb72e45f65ff0503fc85
Yanako Lajara <yanakolajara@pursuit.org>
2023-06-23 14:27:56 -0400

    files name fix

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
-const expess = require('express')
+const express = require('express')
 const logger = require('morgan')
 
 const app = express()
+const locations = require('./controllers/locations.controller')
+const  machines = require('./controllers/machines.controller')
+const persons = require('./controllers/persons.controller')
+const plan = require('./controllers/')
+const  = require('./controllers/')
+
+app.use(express.json())
+app.use(logger('dev'))
+app.use(express.Router())
+
+app.use
 
 module.exports = app;
