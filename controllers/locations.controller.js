293be0b74f71ad3bd56690002f81dd6d78624cf6
Michelle Harley <chell.hx@gmail.com>
2023-06-25 00:57:42 -0400

    ✅, bonus attempt but ❌

--- controllers/locations.controller.js
+++ controllers/locations.controller.js
@@ -2,9 +2,34 @@ const express = require("express");
 const router = express.Router();
 
 const locations = require("../models/location.model");
+const peopleData = require("../models/person.model");
 
 router.get("/", (req, res) => {
   res.send(locations);
 });
+
+// - - - BONUS (FAIL) - - -
+// router.get("/people", (req, res) => {
+//   const locationsWithPeople = [...locations];
+//   locationsWithPeople.forEach(({ zip }) => {
+//     peopleData.forEach((person) => {
+//       if (person.mainLocation === zip) {
+//         locationsWithPeople.people = person;
+//        });
+//     res.send(locationsWithPeople);
+//
+// });
+
+// basic understanding
+// // people.forEach((person) => {
+// //   if (person.mainLocation === zip) {
+// //     locations.people = person;
+// //   }
+
+// extreme basic
+// //   if (obj.zip === "11011") {
+// //     obj.members = members;
+// //   }
+// // });
 
 module.exports = router;
