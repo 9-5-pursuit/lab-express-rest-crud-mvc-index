1cddb28c4f995992dfd98b919fe1807610b05ce8
Jennifer <jenniferpeterson@pursuit.org>
2023-06-26 08:45:59 -0400

    start up express app

---
+++ app.js
@@ -0,0 +1,13 @@
+// DEPENDENCIES
+const express = require("express");
+
+// CONFIGURATION
+const app = express();
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.send("Hello, world");
+});
+
+// EXPORT
+module.exports = app;
