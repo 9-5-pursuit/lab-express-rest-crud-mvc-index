bf71e74f4048f19934e26c84d9ccb3d7ebf675ce
Naomi-Pate <patenaomi99@gmail.com>
2023-06-26 09:31:02 -0400

    fixed typo '3001' on server.js line 7 to '3333'

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ require("dotenv").config();
 const PORT = 3333 || process.env.PORT;
 
 app.listen(3333, () => {
-  console.log("Server is now running on port " + 3001);
+  console.log("Server is now running on port " + 3333);
 });
