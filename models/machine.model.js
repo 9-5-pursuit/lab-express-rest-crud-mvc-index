b12de9272bf1214b028f41a9460e356eceff4ec5
daleskiii <aun2793@gmail.com>
2023-07-06 01:02:13 -0400

    lab complete

---
+++ models/machine.model.js
@@ -0,0 +1,24 @@
+const machines = [
+  {
+    brand: "Bowflex",
+    model: "Bowflexer 4000",
+    type: "Extreme Fitness Bench",
+    purchased: 1986,
+    location: 11101,
+  },
+  {
+    brand: "Bowflex",
+    model: "Bowflexer 4000",
+    type: "Extreme Fitness Bench",
+    purchased: 1996,
+    location: 11101,
+  },
+  {
+    brand: "Bowflex",
+    model: "Bowflexer 4000",
+    type: "Extreme Fitness Bench",
+    purchased: 2006,
+    location: 10011,
+  },
+];
+module.exports = machines;
